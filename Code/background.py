0cf8fda2a5ea7d28e1c51a2b6e234119e1126830
Connor Schutze <connorschutze@gmail.com>
2022-06-01 12:26:44 -0700

    Comments.

--- Code/background.py
+++ Code/background.py
@@ -9,6 +9,8 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.sprite_type = sprite_type
         self.image = surface
+        
+        # Object is for graphics larger than one tile
         if self.sprite_type == "object":
             self.rect = self.image.get_rect(topleft = [pos[0], pos[1] - TILESIZE])
         else:
