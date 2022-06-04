14534767041d71d82599140f9ffe99e6c3272a30
ZacOhran <zac.ohran@gmail.com>
2022-06-03 22:18:30 -0700

    stats

--- Code/stats.py
+++ Code/stats.py
@@ -1,5 +1,5 @@
 TROOP_STATS = {
-    'Knight' : {'HP' : 1000, 'ATK' : 10, 'DEF' : 5, 'MGA' : 0, 'MGD' : 5, 'MGP' : 0, 'SPD' : 5, 'CRIT' : 20},
+    'Knight' : {'HP' : 1000, 'ATK' : 10, 'DEF' : 5, 'MGA' : 0, 'MGD' : 5, 'MGP' : 0, 'SPD' : 5, 'CRIT' : 20, 'TEMPHP' : 1000, 'TEMPATK' : 10, 'TEMPDEF' : 5, 'TEMPMGA' : 0, 'TEMPMGD' : 5, 'TEMPMGP' : 0, 'TEMPSPD' : 5, 'TEMPCRIT' : 20},
     'Knight1' : {'HP' : 1000, 'ATK' : 10, 'DEF' : 5, 'MGA' : 0, 'MGD' : 5, 'MGP' : 0, 'SPD' : 5, 'CR' : 20 },
     'Knight2' : {'HP' : 1000, 'ATK' : 10, 'DEF' : 5, 'MGA' : 0, 'MGD' : 5, 'MGP' : 0, 'SPD' : 5, 'CR' : 20 },
     'Knight3' : {'HP' : 1000, 'ATK' : 10, 'DEF' : 5, 'MGA' : 0, 'MGD' : 5, 'MGP' : 0, 'SPD' : 5, 'CR' : 20 }
